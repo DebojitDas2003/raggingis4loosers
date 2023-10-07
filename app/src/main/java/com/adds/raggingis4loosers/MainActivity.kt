ecb92e16cc099cac035e9ec57303f46a10c1f0eb
Dsmita03 <debasmitasarkar@2003>
2023-10-07 11:27:56 +0530

    done

--- app/src/main/java/com/adds/raggingis4loosers/MainActivity.kt
+++ app/src/main/java/com/adds/raggingis4loosers/MainActivity.kt
@@ -119,4 +119,178 @@ class MainActivity : AppCompatActivity() {
         fragmentTransaction.replace(R.id.frame_layout, fragment)
         fragmentTransaction.commit()
     }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    class VoiceRecognitionService : Service() {
+        private lateinit var speechRecognizer: SpeechRecognizer
+
+        override fun onCreate() {
+            super.onCreate()
+
+            // Check and request audio recording permission
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.RECORD_AUDIO
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // You should request permission here and handle the result
+                // ActivityCompat.requestPermissions(...)
+            }
+
+            // Create a notification channel (required for Android 8.0+)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    "Voice Recognition Service",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                val notificationManager = getSystemService(NotificationManager::class.java)
+                notificationManager.createNotificationChannel(channel)
+            }
+
+            // Create a notification for the Foreground Service
+            val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Voice Recognition Service")
+                .setContentText("Listening for voice commands...")
+                .setSmallIcon(R.drawable.ic_notification_icon)
+                .build()
+
+            // Start the Foreground Service with the notification
+            startForeground(NOTIFICATION_ID, notification)
+        }
+
+        override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+            // Initialize and start your voice recognition logic here
+            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+            speechRecognizer.setRecognitionListener(object : RecognitionListener {
+                // Implement speech recognition callbacks here
+            })
+
+            // Start listening for voice commands
+            val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            recognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            speechRecognizer.startListening(recognizerIntent)
+
+            return START_STICKY
+        }
+
+        override fun onBind(intent: Intent?): IBinder? {
+            return null
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+
+            // Stop and release the SpeechRecognizer
+            if (::speechRecognizer.isInitialized) {
+                speechRecognizer.destroy()
+            }
+        }
+
+        companion object {
+            private const val CHANNEL_ID = "VoiceRecognitionChannel"
+            private const val NOTIFICATION_ID = 12345
+        }
+    }
+
+
+    class MainActivity : AppCompatActivity() {
+
+        private val REQUEST_RECORD_AUDIO_PERMISSION = 1
+        private lateinit var speechRecognizer: SpeechRecognizer
+        private lateinit var recognizedText: TextView
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val startListeningButton: Button = findViewById(R.id.startListeningButton)
+            recognizedText = findViewById(R.id.recognizedText)
+
+            startListeningButton.setOnClickListener {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.RECORD_AUDIO
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.RECORD_AUDIO),
+                        REQUEST_RECORD_AUDIO_PERMISSION
+                    )
+                } else {
+                    startListening()
+                }
+            }
+        }
+
+        override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+        ) {
+            if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startListening()
+                }
+            }
+        }
+
+        private fun startListening() {
+            // Initialize the speech recognizer and start listening
+            speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this)
+            val recognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            recognizerIntent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+
+            speechRecognizer.setRecognitionListener(object : RecognitionListener {
+                override fun onReadyForSpeech(params: Bundle?) {}
+                override fun onBeginningOfSpeech() {}
+                override fun onRmsChanged(rmsdB: Float) {}
+                override fun onBufferReceived(buffer: ByteArray?) {}
+                override fun onEndOfSpeech() {}
+                override fun onError(errorCode: Int) {}
+
+                override fun onResults(results: Bundle?) {
+                    val data = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+                    if (data != null && data.isNotEmpty()) {
+                        val recognized = data[0]
+                        recognizedText.text = recognized
+                    }
+                }
+
+                override fun onPartialResults(partialResults: Bundle?) {}
+                override fun onEvent(eventType: Int, params: Bundle?) {}
+            })
+
+            speechRecognizer.startListening(recognizerIntent)
+        }
+
+        override fun onDestroy() {
+            super.onDestroy()
+            if (::speechRecognizer.isInitialized) {
+                speechRecognizer.destroy()
+            }
+        }
+    }
+
+
+
 }
+
+
+
+
