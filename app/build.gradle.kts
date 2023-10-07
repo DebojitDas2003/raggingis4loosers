703b9c23a847a08712d3dcf877eff2cfa2954b05
DebojitDas2003 <debojitdas0110@gmail.com>
2023-10-07 10:03:57 +0530

    push karo khush raho

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,9 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+            implementation ("androidx.appcompat:appcompat:1.6.1")
+            implementation ("com.google.android.material:material:1.10.0")
+            implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
