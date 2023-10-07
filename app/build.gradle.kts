ed00619758e08810c97e31287c4aed35ffab5c3f
srijanid <srijanidas06@gmail.com>
2023-10-07 16:08:34 +0530

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
             implementation ("androidx.appcompat:appcompat:1.6.1")
             implementation ("com.google.android.material:material:1.10.0")
@@ -66,6 +65,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.firebase:firebase-auth:22.1.2")
     implementation("com.google.firebase:firebase-firestore:24.8.1")
+    implementation("androidx.preference:preference:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
